492650a03d39863b939b265ff38f3f3e87d7efb1
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2013-10-15 12:38:39 +0100

    5: red. Does not computes correctly

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
   * [Behavior-driven testing with RSpec
   A comprehensive approach to test-driven development](https://www.ibm.com/developerworks/web/library/wa-rspec/)
   * [An Introduction to RSpec](http://maxwoghiren.com/blog/?p=117)
+  * [Testing Tuesday #9: Stubbing and Mocking with RSpec](http://blog.codeship.io/2013/06/11/testing-tuesday-9-stubbing-and-mocking-with-rspec.html)
--- spec/math/rpcalc_spec.rb
+++ spec/math/rpcalc_spec.rb
@@ -15,5 +15,14 @@ module Math
         expect { @e[] }.to raise_error("Error. found 'a'. Expected number or operator")
       end
     end
-  end
-end
+
+    context "When a correct input is given" do
+      it "must give the correct answer" do
+        r = @c.calc('2 3 + 4 *')
+        r.should eq 20
+        @c.stack.should eq []
+      end
+    end
+
+  end    # RPCalc
+end      # Math
