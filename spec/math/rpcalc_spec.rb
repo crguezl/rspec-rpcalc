523b819b6bd1250d8b96a4e6fa251db02bd00d6a
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2013-10-15 11:10:20 +0100

    2: red. Bad input requirement

--- spec/math/rpcalc_spec.rb
+++ spec/math/rpcalc_spec.rb
@@ -2,6 +2,15 @@
 require "spec_helper"
 module Math
   describe RPCalc do
+    before :each do
+      @c = Math::RPCalc.new
+    end
 
+    context "When an erroneous input is given" do
+      it "must raise an exception" do
+        expect { @c.calc('a') }.to raise_error(SyntaxError)
+        expect { @c.calc('a') }.to raise_error("Error. found 'a'. Expected number or operator")
+      end
+    end
   end
 end
