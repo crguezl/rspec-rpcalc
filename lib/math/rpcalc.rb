0fb68c5abbc79bfb337ce5fcbbbdccde17fd15bc
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2013-10-15 11:36:33 +0100

    3: green. Bad input detected

--- lib/math/rpcalc.rb
+++ lib/math/rpcalc.rb
@@ -1,5 +1,19 @@
 module Math
   class RPCalc
+    attr_accessor :stack
+
+    def initialize()
+      @stack = []
+    end
+
+    def calc(expr)
+      @stack = []
+      expr.split(/\s+/).each do |x|
+        raise SyntaxError, "Error. found '#{x}'. Expected number or operator" 
+      end
+      @stack.pop
+    end
+
   end  # class RPCalc
 end    # Math
 
